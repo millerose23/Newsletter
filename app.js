c121616c66cc83912c0254f24ff72de3c0e4d098
unknown <you@example.com>
2019-12-15 19:20:19 +0200

    newsletter v5 (removed keys from app)

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ var data = {
 var jsonData = JSON.stringify(data);
 
 var options = {
-  url:  "https://us18.api.mailchimp.com/3.0/lists/7f68043477",
+  url:  "https://us18.api.mailchimp.com/3.0/lists/listnumber",
   method: "POST",
   headers: {
-  "Authorization":"millerose23 c4edfb0962659e705a60185a69ed774b-us18 "
+  "Authorization":"username apikey"
 },
   body: jsonData
 };
@@ -43,20 +43,11 @@ request(options, function(error, response, body){
 if (error){
   res.sendFile(__dirname + "/failure.html");
 } else {
-  if (response.statusCode == 200)
-  {
-    res.sendFile(__dirname + "/sucess.html");
-  }else{
-    res.sendFile(__dirname + "/failure.html");
-  }
+  res.sendFile(__dirname + "/sucess.html");
 }
 });
 });
-app.post("failure", function(req,res){
-  res.redirect("/");
-});
-
-//local or dynamic
+//local or server
 app.listen(process.env.PORT || 3000, function() {
   console.log("server is running");
 });
